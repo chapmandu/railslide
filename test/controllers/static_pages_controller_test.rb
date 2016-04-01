ca51c6234321f2554b2d8f381262cae5d5fc65b7
Adam Chapman <adam.p.chapman@gmail.com>
2016-04-01 21:55:31 +1100

    progress commit

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,18 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test 'should get home' do
+  test 'should get root' do
     get :home
     assert_response :success
+    assert_select 'title', 'Railslide'
   end
 
   test 'should get help' do
     get :help
     assert_response :success
+    assert_select 'title', 'Railslide :: Help'
   end
 
   test 'should get about' do
     get :about
     assert_response :success
+    assert_select 'title', 'Railslide :: About'
   end
 end
