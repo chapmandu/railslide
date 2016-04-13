b581d72083f7e558058184cdec6c537902ed73d4
Adam Chapman <adam.p.chapman@gmail.com>
2016-04-14 08:47:17 +1000

    progress commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,10 @@ class User < ActiveRecord::Base
   validates(:password, presence: true, length: { minimum: 6 })
   # other stuff
   has_secure_password
+
+  # Returns the hash digest of the given string.
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
